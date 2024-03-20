031d689418b36922029ec89a2c906367c5e345fd
calvinkalin <ilya.muromec888@gmail.com>
2024-03-20 16:35:07 +0300

    Sprint 8

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -10,4 +10,15 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var contentImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        contentImage.layer.cornerRadius = 10
+        contentImage.layer.masksToBounds = true
+    }
+    
 }
